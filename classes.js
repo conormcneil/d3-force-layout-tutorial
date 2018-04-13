b35017f7c3d14fc61b3552d79dad48a2708edc13
Conor Kingston <cmkingston21@me.com>
2018-04-13 12:46:13 -0700

    added attributes to classes.js

--- classes.js
+++ classes.js
@@ -132,4 +132,58 @@ let _classes = [{
         "minimum_occurrences": ["1"],
         "maximum_occurrences": ["1"]
     }]
-}]
+},
+// DD_Attribute
+{
+    "name": ["axis_number"],
+    "version_id": ["1.0"],
+    "local_identifier": ["particle.axis_number"],
+    "nillable_flag": ["false"],
+    "submitter_name": ["Todd King"],
+    "definition": ["The index of the axis. The first axis is numbered as 1."],
+    "DD_Value_Domain": [{
+        "enumeration_flag": ["false"],
+        "value_data_type": ["ASCII_Integer"],
+        "unit_of_measure_type": ["Units_of_None"],
+        "minimum_value": ["1"]
+    }]
+}, {
+    "name": ["face_axis"],
+    "version_id": ["1.0"],
+    "local_identifier": ["particle.face_axis"],
+    "nillable_flag": ["false"],
+    "submitter_name": ["Todd King"],
+    "definition": ["The axis number that defines one axis of a face plane."],
+    "DD_Value_Domain": [{
+        "enumeration_flag": ["false"],
+        "value_data_type": ["ASCII_Integer"],
+        "unit_of_measure_type": ["Units_of_None"],
+        "minimum_value": ["1"]
+    }]
+}, {
+    "name": ["local_reference_type"],
+    "version_id": ["1.0"],
+    "local_identifier": ["particle.local_reference_type"],
+    "nillable_flag": ["false"],
+    "submitter_name": ["Todd King"],
+    "definition": ["The local_reference_type attribute provides the name of an association between an entity identified by a local_identifier_reference and another corresponding entity identified by a local_identifier"],
+    "DD_Value_Domain": [{
+        "enumeration_flag": ["true"],
+        "value_data_type": ["ASCII_Short_String_Collapsed"],
+        "unit_of_measure_type": ["Units_of_None"],
+        "DD_Permissible_Value": [{
+            "value": ["particle_observation_to_observation_values"],
+            "value_meaning": ["A reference to the array containing the primary observation data."]
+        }, {
+            "value": ["particle_observation_to_axis_values"],
+            "value_meaning": ["A reference to the array containing the data values for an axis of primary observation data."]
+        }, {
+            "value": ["particle_observation_to_face_values"],
+            "value_meaning": ["A reference to the array containing the data values for a face of primary observation data."]
+        }, {
+            "value": ["particle_observation_to_aligned_values"],
+            "value_meaning": ["A reference to an array that has an identical array structure (in both axis definitions, and number of elements) as the primary observation data array."]
+        }]
+    }]
+}];
+
