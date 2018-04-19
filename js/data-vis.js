94799cd98512c81b90a1f9073f9246b1a78f695a
Conor Kingston <cmkingston21@me.com>
2018-04-19 12:33:30 -0700

    beautified, simplified variables

--- js/data-vis.js
+++ js/data-vis.js
@@ -1,38 +1,36 @@
 var width = $(document).width() - 10,
     height = $(document).height() - 10,
-    radius = 20,
     columnCount,
     activeNode = null,
     _col = 1, // root elements exist in this column
-    
+
     // Edges (Lines)
-    lineHighlightFill = 'pink',
+    lineHighlightFill = 'orange',
     lineHighlightWidth = 5,
-    lineFill = 'black',
-    lineWidth = 1,
-    
+    lineStroke = 'black',
+    lineStrokeWidth = 1,
+
     // Nodes
-    nodeHighlightFill = 'pink',
+    rx = 100, // x radius of ellipse
+    ry = 30, // y radius of ellipse
+    verticalPadding = 5,
+    verticalSpacing = ry * 2 + verticalPadding,
+    nodeHighlightFill = 'orange',
     rootNodeFill = 'lightgreen',
     classNodeFill = 'lightblue',
     attributeNodeFill = 'white',
+    nodeStroke = 'black',
     activeNodes = [],
-    nodeGen = [],
-    
-    // Force layout configuration
-    forceCharge = -3000,
-    forceLinkStrength = 0.5;
+    nodeGen = [];
 
 
 var svg = d3.select('body').append('svg')
     .attr('width', width)
     .attr('height', height)
     .call(d3.behavior.zoom().on("zoom", function() {
-        svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
+        svg.attr("transform", `translate(${d3.event.translate}) scale(${d3.event.scale})`)
     }))
     .append('g');
-
-var fill = d3.scale.category20();
 
 function initForce() {
 
@@ -40,16 +38,16 @@ function initForce() {
     activeNode = null;
 
     var force = d3.layout.force()
-        .charge(forceCharge)
-        .linkStrength(forceLinkStrength)
-        .size([width, height]);
+        .size([width, height])
+        .nodes(dataNodes)
+        .links(dataLinks);
 
     var links = svg.selectAll('.link')
         .data(dataLinks)
         .enter().append('line')
         .attr('class', 'link')
-        .style('stroke-width',2)
-        .style('stroke',lineFill);
+        .style('stroke-width', lineStrokeWidth)
+        .style('stroke', lineStroke);
 
     var nodes = svg.selectAll('g')
         .data(dataNodes)
@@ -58,25 +56,27 @@ function initForce() {
         .on('click', toggleNodes)
         .attr('id', function(d) {
             let _id;
-            
+
             try {
                 _id = d['local_identifier'][0].replace('.', '-');
             } catch (e) {
                 _id = d['identifier_reference'][0].replace('.', '-');
             }
-            
-            
+
             return _id;
-        })
+        });
+
+    // configure class for each node
+    // corresponding to its position relative to root node
+    // this is used later for *horizontal* positioning of the nodes
+    nodeClasses(rootNodes);
 
     nodes
         .append('ellipse')
-        .attr('rx', 5*radius)
-        .attr('ry',   radius)
+        .attr('rx', rx)
+        .attr('ry', ry)
         .attr('class', 'circle')
-        .style('stroke', function(d) {
-            return d3.rgb(fill(d.group)).darker();
-        })
+        .style('stroke', nodeStroke)
         .style('fill', highlightNode);
 
     nodes
@@ -89,65 +89,54 @@ function initForce() {
         })
         .text(function(d) {
             return d.name[0];
+        });
+
+    nodes
+        .each(function(d, idx) {
+            // configure horiontal (x) position
+            let className = d3.select(this).attr('class');
+            let colWidth = width / _col;
+
+            if (d.rootNode) d.x = colWidth;
+            else if (/col-/g.test(className)) {
+                let col = className.match(/[0-9]/g).join('');
+                d.x = col * colWidth;
+            };
+
+            // configure vertical (y) position
+            d.y = 300 + idx * verticalSpacing;
         })
+        .attr('x1', function(d) {
+            return d.x;
+        })
+        .attr('y1', function(d) {
+            return d.y;
+        });
 
-    force
-        .nodes(dataNodes)
-        .links(dataLinks)
-        .on('tick', tick)
-        .start();
+    force.start();
 
-    let firstTick = true;
-
-    function tick(e) {
-
-        var k = 10 * e.alpha;
-
-        nodes
-            .each(function(d, idx) {
-                let className = d3.select(this).attr('class');
-                let colWidth = 1.5*width / _col;
-            
-                if (d.rootNode) return d.x = colWidth;
-                else if (/col-/g.test(className)) {
-                    let col = className.match(/[0-9]/g).join('');
-                    return d.x = col * colWidth;
-                };
-            })
-            .attr('x1', function(d) {
-                return d.x;
-            })
-            .attr('y1', function(d) {
-                return d.y;
-            })
-
-        links
-            .attr('x1', function(d) {
-                return d.source.x;
-            })
-            .attr('y1', function(d) {
-                return d.source.y;
-            })
-            .attr('x2', function(d) {
-                return d.target.x;
-            })
-            .attr('y2', function(d) {
-                return d.target.y;
-            })
-
-        svg.selectAll('.node')
-            .attr('transform', function(d) {
-                let x = d.x;
-                let y = d.y;
-                return `translate(${x},${y}) rotate(0)`;
-            });
-
-        if (firstTick) {
-            firstTick = false;
-            nodeClasses(rootNodes);
-        };
-
-    };
+    // position nodes and links
+    // NODES
+    nodes
+        .attr('transform', function(d) {
+            let x = d.x;
+            let y = d.y;
+            return `translate(${x},${y}) rotate(0)`;
+        });
+    // LINKS
+    links
+        .attr('x1', function(d) {
+            return d.source.x;
+        })
+        .attr('y1', function(d) {
+            return d.source.y;
+        })
+        .attr('x2', function(d) {
+            return d.target.x;
+        })
+        .attr('y2', function(d) {
+            return d.target.y;
+        });
 };
 
 // set node classes for use later during positioning
@@ -158,9 +147,6 @@ function nodeClasses(_nodes) {
 
     // for each root element, find its child elements
     _nodes.map(root => {
-        // this array (_children) is not the same as the dataNodes array
-        // values that exist here must be mapped to dataNodes elements
-        // and node classes are added there
         let _children = root['DD_Association'];
 
         // check that each child exists as an element in dataNodes
@@ -189,13 +175,13 @@ function nodeClasses(_nodes) {
                     // to be passed into recursive function upon completion of find() method
                     if (_match) {
                         let _lid;
-                        
+
                         try {
                             _lid = dn['local_identifier'][0].replace('.', '-');
                         } catch (e) {
                             _lid = dn['identifier_reference'][0].replace('.', '-');
                         }
-                        
+
                         let _localNode = d3.select(`#${_lid}`)
                             .attr('class', function(d) {
                                 // console.log(d);
@@ -213,7 +199,7 @@ function nodeClasses(_nodes) {
             });
         }
     });
-    
+
     if (nextCol.length) nodeClasses(nextCol);
 };
 
@@ -223,7 +209,7 @@ function toggleNodes(node) {
     let g1 = node['local_identifier'];
     let g2 = node['children'];
     let g3 = getNextGen(g2);
-    
+
     if (activeNode == node) {
         activeNode = null;
         nodeGen = [];
@@ -234,48 +220,48 @@ function toggleNodes(node) {
             .concat(g2)
             .concat(g3);
     }
-    
+
     svg.selectAll('.link')
-        .style('stroke',highlightLine);
-    
+        .style('stroke', highlightLine);
+
     svg.selectAll('.circle')
-        .style('fill',highlightNode);
+        .style('fill', highlightNode);
 };
 
 function highlightLine(_link) {
     let _lid = _link.source['local_identifier'][0];
-    
+
     if (nodeGen.indexOf(_lid) != -1) return lineHighlightFill;
-    else return lineFill;
+    else return lineStroke;
 };
 
 function highlightNode(n) {
     let _color,
         _lid;
-    
+
     try {
         _lid = n['local_identifier'][0];
     } catch (e) {
         _lid = n['identifier_reference'][0];
     }
-    
+
     if (activeNodes.indexOf(_lid) != -1) _color = nodeHighlightFill;
     else if (n.rootNode) _color = rootNodeFill;
     else if (n.className == 'class') _color = classNodeFill;
     else _color = attributeNodeFill;
-    
+
     return _color;
 };
 
 function getNextGen(gen) {
     let _nextGen = [];
-    
+
     dataLinks.map(link => {
         let _sourceLid = link.source['local_identifier'][0];
         let _targetLid = link.target['local_identifier'][0];
-        
+
         if (gen && gen.indexOf(_sourceLid) != -1 && _nextGen.indexOf(_targetLid) == -1) _nextGen.push(_targetLid);
     });
-    
+
     return _nextGen;
 };
